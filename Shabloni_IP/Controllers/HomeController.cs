af9c970ebe4145c2a9a787ac93d51df75167c9f1
Ilijan69 <fooltiktok06@gmail.com>
2024-11-26 08:14:48 +0200

    Shabloni

---
+++ Shabloni_IP/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Shabloni_IP.Models;
+using System.Diagnostics;
+
+namespace Shabloni_IP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SocialMedis()
+        {
+            return View();
+        }
+
+        public IActionResult Termsconditions()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
